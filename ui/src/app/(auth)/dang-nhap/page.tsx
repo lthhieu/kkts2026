f3e250b4602f8b115aba14b0b9dd3d4e890fb418
Ly Tran Hoang Hieu <lyhieu2024@gmail.com>
2026-02-25 07:19:26 +0700

    redirect to home when logined

--- ui/src/app/(auth)/dang-nhap/page.tsx
+++ ui/src/app/(auth)/dang-nhap/page.tsx
@@ -1,8 +1,13 @@
 import { auth } from '@/auth';
 import LoginComponent from '@/components/auth/login';
+import { redirect } from 'next/navigation';
 
 const Login = async () => {
     const session = await auth()
+    if (session?.access_token) {
+        console.log('Bạn đã đăng nhập, chuyển hướng đến trang chủ...');
+        redirect('/quan-tri/trang-chu');
+    }
     // console.log('Session:', session);
     return <LoginComponent />
 };
